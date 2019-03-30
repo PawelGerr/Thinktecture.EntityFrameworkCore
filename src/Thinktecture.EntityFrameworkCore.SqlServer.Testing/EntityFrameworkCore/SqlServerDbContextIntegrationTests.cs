b3bae3723a9d28aeb4392944e133b6e21b9b8f83
Pawel Gerr <pawel.gerr@thinktecture.com>
2019-03-30 13:43:04 +0100

    SqlServerDbContextIntegrationTests can be provided with an ILoggerFactory. Pu a lock around the migrations because EF does not prevent parallel execution of them. Added comments.

--- src/Thinktecture.EntityFrameworkCore.SqlServer.Testing/EntityFrameworkCore/SqlServerDbContextIntegrationTests.cs
+++ src/Thinktecture.EntityFrameworkCore.SqlServer.Testing/EntityFrameworkCore/SqlServerDbContextIntegrationTests.cs
@@ -1,19 +1,29 @@
 using System;
+using System.Collections.Concurrent;
 using System.Data;
 using JetBrains.Annotations;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.Logging;
 using Thinktecture.EntityFrameworkCore.Infrastructure;
 using Thinktecture.EntityFrameworkCore.Migrations;
 
 namespace Thinktecture.EntityFrameworkCore
 {
+   /// <summary>
+   /// A base class for integration tests using EF Core along with SQL Server.
+   /// </summary>
+   /// <typeparam name="T">Type of the database context.</typeparam>
+   // ReSharper disable once UnusedMember.Global
    public abstract class SqlServerDbContextIntegrationTests<T> : IDisposable
       where T : DbContext
    {
       private const string _HISTORY_TABLE_NAME = "__EFMigrationsHistory";
+
+      // ReSharper disable once StaticMemberInGenericType because the locks are all for the same database context but for different schemas.
+      private static readonly ConcurrentDictionary<string, object> _locks = new ConcurrentDictionary<string, object>(StringComparer.OrdinalIgnoreCase);
 
       private readonly string _connectionString;
       private readonly bool _useSharedTables;
@@ -21,26 +31,61 @@ namespace Thinktecture.EntityFrameworkCore
       private string _schema;
       private T _dbContext;
       private IDbContextTransaction _tx;
+      private ILoggerFactory _loggerFactory;
 
+      /// <summary>
+      /// Database schema in use.
+      /// </summary>
       [NotNull]
+      // ReSharper disable once MemberCanBePrivate.Global
       protected string Schema => _schema ?? (_schema = GetSchema(_useSharedTables));
 
+      /// <summary>
+      /// Database context being used for the tests.
+      /// </summary>
       [NotNull]
+      // ReSharper disable once UnusedMember.Global
       protected T DbContext => _dbContext ?? (_dbContext = CreateContext());
 
+      /// <summary>
+      /// Initializes a new instance of <see cref="SqlServerDbContextIntegrationTests{T}"/>
+      /// </summary>
+      /// <param name="connectionString">Connection string to use.</param>
+      /// <param name="useSharedTables">Indication whether new tables with a new schema should be created or not.</param>
       protected SqlServerDbContextIntegrationTests([NotNull] string connectionString, bool useSharedTables)
       {
          _connectionString = connectionString ?? throw new ArgumentNullException(nameof(connectionString));
          _useSharedTables = useSharedTables;
       }
 
+      /// <summary>
+      /// Creates a new instance of the database context.
+      /// </summary>
+      /// <param name="options">Options to use for creation.</param>
+      /// <param name="schema">Database schema to use.</param>
+      /// <returns>A new instance of the database context.</returns>
       [NotNull]
       protected abstract T CreateContext([NotNull] DbContextOptions<T> options, [NotNull] IDbContextSchema schema);
 
+      /// <summary>
+      /// Gets/generates schema to be used.
+      /// </summary>
+      /// <param name="useSharedTables">Indication whether a new schema should be generated or a shared one.</param>
+      /// <returns>A database schema.</returns>
       [NotNull]
       protected virtual string GetSchema(bool useSharedTables)
       {
          return useSharedTables ? "tests" : Guid.NewGuid().ToString("N");
+      }
+
+      /// <summary>
+      /// Sets or resets the logger factory to be used by EF.
+      /// </summary>
+      /// <param name="loggerFactory">Logger factory to use.</param>
+      // ReSharper disable once UnusedMember.Global
+      protected void UseLoggerFactory([CanBeNull] ILoggerFactory loggerFactory)
+      {
+         _loggerFactory = loggerFactory;
       }
 
       [NotNull]
@@ -48,7 +93,7 @@ namespace Thinktecture.EntityFrameworkCore
       {
          var optionsBuilder = CreateOptionsBuilder(_connectionString);
          var ctx = CreateContext(optionsBuilder.Options, new DbContextSchema(Schema));
-         ctx.Database.Migrate();
+         RunMigrations(ctx);
 
          if (_useSharedTables)
             _tx = ctx.Database.BeginTransaction(IsolationLevel.ReadCommitted);
@@ -56,19 +101,52 @@ namespace Thinktecture.EntityFrameworkCore
          return ctx;
       }
 
+      /// <summary>
+      /// Runs migrations for provided <paramref name="ctx" />.
+      /// </summary>
+      /// <param name="ctx">Database context to run migrations for.</param>
+      /// <exception cref="ArgumentNullException">The provided context is <c>null</c>.</exception>
+      protected virtual void RunMigrations([NotNull] T ctx)
+      {
+         if (ctx == null)
+            throw new ArgumentNullException(nameof(ctx));
+
+         // concurrent execution is not supported by EF migrations
+         lock (_locks.GetOrAdd(Schema, s => new object()))
+         {
+            ctx.Database.Migrate();
+         }
+      }
+
+      /// <summary>
+      /// Creates and configures the <see cref="DbContextOptionsBuilder{TContext}"/>
+      /// </summary>
+      /// <param name="connString">Database connection string</param>
+      /// <returns>An instance of <see cref="DbContextOptionsBuilder{TContext}"/></returns>
+      /// <exception cref="ArgumentNullException"><paramref name="connString"/> is null.</exception>
       [NotNull]
       protected virtual DbContextOptionsBuilder<T> CreateOptionsBuilder([NotNull] string connString)
       {
          if (connString == null)
             throw new ArgumentNullException(nameof(connString));
 
-         return new DbContextOptionsBuilder<T>()
-                .UseSqlServer(connString, ConfigureSqlServer)
-                .ReplaceService<IMigrationsSqlGenerator, ThinktectureSqlServerMigrationsSqlGenerator>()
-                .ReplaceService<IMigrationsAssembly, DbSchemaAwareMigrationAssembly>()
-                .ReplaceService<IModelCacheKeyFactory, DbSchemaAwareModelCacheKeyFactory>();
+         var builder = new DbContextOptionsBuilder<T>()
+                       .UseSqlServer(connString, ConfigureSqlServer)
+                       .ReplaceService<IMigrationsSqlGenerator, ThinktectureSqlServerMigrationsSqlGenerator>()
+                       .ReplaceService<IMigrationsAssembly, DbSchemaAwareMigrationAssembly>()
+                       .ReplaceService<IModelCacheKeyFactory, DbSchemaAwareModelCacheKeyFactory>();
+
+         if (_loggerFactory != null)
+            builder.UseLoggerFactory(_loggerFactory);
+
+         return builder;
       }
 
+      /// <summary>
+      /// Configures SQL Server options.
+      /// </summary>
+      /// <param name="builder">A builder for configuration of the options.</param>
+      /// <exception cref="ArgumentNullException">The <paramref name="builder"/> is null.</exception>
       protected virtual void ConfigureSqlServer([NotNull] SqlServerDbContextOptionsBuilder builder)
       {
          if (builder == null)
@@ -77,6 +155,10 @@ namespace Thinktecture.EntityFrameworkCore
          builder.MigrationsHistoryTable(_HISTORY_TABLE_NAME, Schema);
       }
 
+      /// <summary>
+      /// Rollbacks transaction if shared tables are used
+      /// otherwise the migrations are rolled back and all tables, functions, views and the newly generated schema are deleted.
+      /// </summary>
       public virtual void Dispose()
       {
          if (_dbContext == null)
