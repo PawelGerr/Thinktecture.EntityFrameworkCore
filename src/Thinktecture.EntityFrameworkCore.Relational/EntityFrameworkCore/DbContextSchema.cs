d9108cb73613258b80645a1aa7944b4879010b0f
Pawel Gerr <pawel.gerr@thinktecture.com>
2019-04-05 08:35:37 +0200

    Renamed project from Thinktecture.EntityFrameworkCore to Thinktecture.EntityFrameworkCore.Relational

--- src/Thinktecture.EntityFrameworkCore.Relational/EntityFrameworkCore/DbContextSchema.cs
+++ src/Thinktecture.EntityFrameworkCore.Relational/EntityFrameworkCore/DbContextSchema.cs
@@ -3,10 +3,20 @@ using JetBrains.Annotations;
 
 namespace Thinktecture.EntityFrameworkCore
 {
+   /// <summary>
+   /// DB schema.
+   /// </summary>
    public class DbContextSchema : IDbContextSchema
    {
+      /// <summary>
+      /// Database schema
+      /// </summary>
       public string Schema { get; }
 
+      /// <summary>
+      /// Initializes new instance of <see cref="DbContextSchema"/>.
+      /// </summary>
+      /// <param name="schema"></param>
       public DbContextSchema([NotNull] string schema)
       {
          Schema = schema ?? throw new ArgumentNullException(nameof(schema));
