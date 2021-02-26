f1f458cd02329189bf985e3a1025092dcf64df12
Pawel Gerr <pawel.gerr@thinktecture.com>
2021-02-26 15:25:25 +0100

    IBulkInsertOrUpdateOptions is not extiending  IBulkInsertOptions/IBulkUpdateOptions anymore because it messes up the intellisense

--- src/Thinktecture.EntityFrameworkCore.BulkOperations/EntityFrameworkCore/BulkOperations/IBulkInsertOrUpdateOptions.cs
+++ src/Thinktecture.EntityFrameworkCore.BulkOperations/EntityFrameworkCore/BulkOperations/IBulkInsertOrUpdateOptions.cs
@@ -3,7 +3,24 @@ namespace Thinktecture.EntityFrameworkCore.BulkOperations
    /// <summary>
    /// Options for bulk insert or update.
    /// </summary>
-   public interface IBulkInsertOrUpdateOptions : IBulkInsertOptions, IBulkUpdateOptions
+   public interface IBulkInsertOrUpdateOptions
    {
+      /// <summary>
+      /// Properties to insert.
+      /// If the <see cref="PropertiesToInsert"/> is null then all properties of the entity are going to be inserted.
+      /// </summary>
+      IEntityPropertiesProvider? PropertiesToInsert { get; }
+
+      /// <summary>
+      /// Properties to update.
+      /// If the <see cref="PropertiesToUpdate"/> is <c>null</c> then all properties of the entity are going to be updated.
+      /// </summary>
+      IEntityPropertiesProvider? PropertiesToUpdate { get; }
+
+      /// <summary>
+      /// Properties to perform the JOIN/match on.
+      /// The primary key of the entity is used by default.
+      /// </summary>
+      IEntityPropertiesProvider? KeyProperties { get; }
    }
 }
