d098604a0133327c00474b6c27b9a557ad376861
Vadym Kostyrkin <vadym.kostyrkin@pekabex.pl>
2023-11-09 09:27:06 +0100

    Custom mod

--- src/Thinktecture.EntityFrameworkCore.Relational/Extensions/RelationalSelectExpressionExtensions.cs
+++ src/Thinktecture.EntityFrameworkCore.Relational/Extensions/RelationalSelectExpressionExtensions.cs
@@ -1,3 +1,5 @@
+using System.Collections.ObjectModel;
+using System.Linq;
 using System.Linq.Expressions;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
@@ -16,42 +18,48 @@ public static class RelationalSelectExpressionExtensions
       this SelectExpression selectExpression,
       IAnnotation annotation)
    {
-      ArgumentNullException.ThrowIfNull(selectExpression);
-      ArgumentNullException.ThrowIfNull(annotation);
+        ArgumentNullException.ThrowIfNull(selectExpression);
+        ArgumentNullException.ThrowIfNull(annotation);
 
-      var tables = selectExpression.Tables;
+        var tables = selectExpression.Tables;
 
-      if (tables.Count == 0)
-         throw new InvalidOperationException($"No tables found to add annotation '{annotation.Name}' to.");
+        if (tables.Count == 0)
+           throw new InvalidOperationException($"No tables found to add annotation '{annotation.Name}' to.");
 
-      if (tables.Count > 1)
-         throw new InvalidOperationException($"Multiple tables found to add annotation '{annotation.Name}' to. Expressions: {String.Join(", ", tables.Select(t => t.Print()))}");
+        //if (tables.Count > 1)
+        //   throw new InvalidOperationException($"Multiple tables found to add annotation '{annotation.Name}' to. Expressions: {String.Join(", ", tables.Select(t => t.Print()))}");
 
-      var tableExpressionBase = selectExpression.Tables[0];
-
-      if (tableExpressionBase is not TableExpression tableExpression)
-         throw new NotSupportedException($"Annotation '{annotation.Name}' can be applied to tables only but found '{tableExpressionBase.GetType().Name}'. Expression: {tableExpressionBase.Print()}");
-
-      return (SelectExpression)new AnnotationApplyingExpressionVisitor(tableExpression, annotation)
+        foreach (var table in tables)
+        {
+            if (table is not TableExpression && table is not LeftJoinExpression && table is not CrossJoinExpression && table is not InnerJoinExpression)
+            {
+                throw new NotSupportedException($"Annotation '{annotation.Name}' can be applied to tables only but found '{table.GetType().Name}'. Expression: {table.Print()}");
+            }
+        }
+        return (SelectExpression)new AnnotationApplyingExpressionVisitor(annotation)
          .Visit(selectExpression)!;
    }
 
    private sealed class AnnotationApplyingExpressionVisitor : ExpressionVisitor
    {
-      private readonly TableExpression _tableExpression;
-      private readonly IAnnotation _annotation;
+        private readonly IAnnotation _annotation;
 
-      public AnnotationApplyingExpressionVisitor(TableExpression tableExpression, IAnnotation annotation)
-      {
-         _tableExpression = tableExpression;
-         _annotation = annotation;
-      }
-
-      public override Expression? Visit(Expression? expression)
-      {
-         return _tableExpression.Equals(expression)
-                   ? _tableExpression.AddAnnotation(_annotation.Name, _annotation.Value)
-                   : base.Visit(expression);
-      }
-   }
+        public AnnotationApplyingExpressionVisitor(IAnnotation annotation)
+        {
+           _annotation = annotation;
+        }
+#nullable enable
+        public override Expression? Visit(Expression? expression)
+        {
+            return base.Visit(expression switch
+            {
+                LeftJoinExpression => (expression as LeftJoinExpression)?.AddAnnotation(_annotation.Name, _annotation.Value),
+                InnerJoinExpression => (expression as InnerJoinExpression)?.AddAnnotation(_annotation.Name, _annotation.Value),
+                CrossJoinExpression => (expression as CrossJoinExpression)?.AddAnnotation(_annotation.Name, _annotation.Value),
+                TableExpression => (expression as TableExpression)?.AddAnnotation(_annotation.Name, _annotation.Value),
+                _ => expression
+            });
+        }
+#nullable disable
+    }
 }
